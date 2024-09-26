2b594290c57a203a068c974b4d7e40ac5145e2ea
IceRussel <baptistecharleton94@gmail.Com>
2024-09-26 15:25:13 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -4,3 +4,8 @@ st.title("Dall-e 3")
 
 # Texte
 st.write("Veuiller entré une description de l'image que vous souhaitez")
+
+# Champ de saisi
+user_input = st.text_input("Tapez votre texte : ")
+
+st.write(user_input)
