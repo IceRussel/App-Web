2a5450b785f19c37a1ee79767d7f6734fc042d00
IceRussel <baptistecharleton94@gmail.Com>
2024-09-26 14:24:56 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,8 +12,8 @@ user_input = st.text_input("Tapez votre texte : ")
 
 st.write(user_input)
 
-# Gif
-st.gif("https://i.makeagif.com/media/6-13-2019/gLyUlA.gif")
-
 # Image
-st.gif("https://i.pinimg.com/originals/47/7a/86/477a86ad9caab74f42e4bf5357b70446.gif")
+st.image("https://i.makeagif.com/media/6-13-2019/gLyUlA.gif")
+
+# Image 2
+st.image("https://i.pinimg.com/originals/47/7a/86/477a86ad9caab74f42e4bf5357b70446.gif")
