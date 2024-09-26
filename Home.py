5e949f43aa9c7daae00a42d830731935d60f0c9a
IceRussel <baptistecharleton94@gmail.Com>
2024-09-26 14:21:31 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,3 +11,6 @@ st.write("Ceci est un formulaire de contrat")
 user_input = st.text_input("Tapez votre texte : ")
 
 st.write(user_input)
+
+# Image
+st.image("https://www.booska-p.com/wp-content/uploads/2022/08/Koba-LaD-Visu-News-1-1024x750.jpg")
