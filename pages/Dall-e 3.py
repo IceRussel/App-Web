e0565b9aa627c64dcd0745a47d206ae2c7fdb2ef
IceRussel <baptistecharleton94@gmail.Com>
2024-09-26 15:50:13 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -6,4 +6,24 @@ st.title("Dall-e 3")
 dalle_input = st.text_input ("Veuiller entré une description de l'image que vous souhaitez générez")
 st.write(dalle_input)
 
-st.sidebar.text_input("Veuillez entré la clé Open IA")
+#Champ de saisie dans la sidebar (pour la clé OpenAI)
+sidebar_input = st.sidebar.text_input("Tapez la clé OpenAI ici :")
+st.write(sidebar_input)
+
+#Intéraction avec OpenAI
+from openai import OpenAI
+client = OpenAI(api_key=sidebar_input)
+
+prompt = "A red dog with a glock 18"
+
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=user_input,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+image_url = image.data[0].url
+
+#Affichage de l'image
+st.image(image_url)
