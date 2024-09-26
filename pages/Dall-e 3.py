69aa044f68c8224a0e3ca580a7694ca40f357911
IceRussel <baptistecharleton94@gmail.Com>
2024-09-26 15:38:12 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -6,4 +6,4 @@ st.title("Dall-e 3")
 dalle_input = st.text_input ("Veuiller entré une description de l'image que vous souhaitez générez")
 st.write(dalle_input)
 
-st.sidebar.text_inout("Veuillez entré la clé Open IA")
+st.sidebar.text_input("Veuillez entré la clé Open IA")
